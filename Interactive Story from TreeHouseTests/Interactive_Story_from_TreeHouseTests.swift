1b134b453fefd6a8bd419d682ba8a1d9b42028c9
Erick Barbosa <erick@Ericks-iMac.local>
2016-08-17 00:03:12 -0400

    Initial Commit

---
+++ Interactive Story from TreeHouseTests/Interactive_Story_from_TreeHouseTests.swift
@@ -0,0 +1,36 @@
+//
+//  Interactive_Story_from_TreeHouseTests.swift
+//  Interactive Story from TreeHouseTests
+//
+//  Created by Erick Barbosa on 8/17/16.
+//  Copyright © 2016 Erick Barbosa. All rights reserved.
+//
+
+import XCTest
+@testable import Interactive_Story_from_TreeHouse
+
+class Interactive_Story_from_TreeHouseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
